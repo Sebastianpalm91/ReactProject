ad81d0a080629531db087e2fb6beb29727825fb5
andrerobsahm <andrerobsahm@users.noreply.github.com>
2018-04-20 12:54:09 +0200

    Merge branch 'master' of https://github.com/Sebastianpalm91/ReactProject

--- magic/src/components/Form.js
+++ magic/src/components/Form.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Form.css";
 
 const Form = props => (
-<<<<<<< HEAD
     <form onSubmit={props.getWeather}>
         <input type="text" name="city" placeholder="city" />
         <input type="text" name="country" placeholder="country" />
@@ -11,16 +10,6 @@ const Form = props => (
             Search Weather
         </button>
     </form>
-=======
-  <form onSubmit={props.getWeather}>
-
-  <input type="text" name="city" placeholder="city"/>
-  <input type="text" name="country" placeholder="country"/>
-
-  <button type="submit">Search Weather</button>
-
-  </form>
->>>>>>> 2c04a9e80b84ed9e00cf5544331987ce05a6454d
 );
 
 export default Form;
