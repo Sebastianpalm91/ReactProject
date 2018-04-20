38442fda5f3bb37650ef4ef15ef4cb2ac4a28f64
andrerobsahm <andrerobsahm@users.noreply.github.com>
2018-04-20 12:52:02 +0200

    Changed form.js and added form.css

--- magic/src/components/Form.js
+++ magic/src/components/Form.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./Form.css";
 
 const Form = props => (
-  <form onSubmit={props.getWeather}>
+    <form onSubmit={props.getWeather}>
+        <input type="text" name="city" placeholder="city" />
+        <input type="text" name="country" placeholder="country" />
 
-    <input type="text" name="city" placeholder="city"/>
-    <input type="text" name="country" placeholder="country"/>
-
-    <button type="submit">Search Weather</button>
-
-  </form>
+        <button className="search-button" type="submit">
+            Search Weather
+        </button>
+    </form>
 );
 
 export default Form;
